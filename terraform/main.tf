009949dd3d3624d01223ba874a86becf20239778
Shovit Roy <shovitroy23@gmail.com>
2025-01-31 22:28:56 +0530

    azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,71 +1,37 @@
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Create EKS Cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.21"
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
 
-# Definition of local variables
-locals {
-  base_apis = [
-    "container.googleapis.com",
-    "monitoring.googleapis.com",
-    "cloudtrace.googleapis.com",
-    "cloudprofiler.googleapis.com"
-  ]
-  memorystore_apis = ["redis.googleapis.com"]
-  cluster_name     = google_container_cluster.my_cluster.name
-}
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
 
-# Enable Google Cloud APIs
-module "enable_google_apis" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 14.0"
-
-  project_id                  = var.gcp_project_id
-  disable_services_on_destroy = false
-
-  # activate_apis is the set of base_apis and the APIs required by user-configured deployment options
-  activate_apis = concat(local.base_apis, var.memorystore ? local.memorystore_apis : [])
-}
-
-# Create GKE cluster
-resource "google_container_cluster" "my_cluster" {
-
-  name     = var.name
-  location = var.region
-
-  # Enabling autopilot for this cluster
-  enable_autopilot = true
-
-  # Setting an empty ip_allocation_policy to allow autopilot cluster to spin up correctly
-  ip_allocation_policy {
+      instance_type = "t3.medium"
+    }
   }
 
-  depends_on = [
-    module.enable_google_apis
-  ]
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
-# Get credentials for cluster
-module "gcloud" {
-  source  = "terraform-google-modules/gcloud/google"
-  version = "~> 3.0"
+# Get credentials for EKS cluster
+module "awscli" {
+  source  = "terraform-aws-modules/cli/aws"
+  version = "~> 1.0"
 
   platform              = "linux"
-  additional_components = ["kubectl", "beta"]
+  additional_components = ["kubectl"]
 
-  create_cmd_entrypoint = "gcloud"
-  # Module does not support explicit dependency
-  # Enforce implicit dependency through use of local variable
-  create_cmd_body = "container clusters get-credentials ${local.cluster_name} --zone=${var.region} --project=${var.gcp_project_id}"
+  create_cmd_entrypoint = "aws"
+  create_cmd_body       = "eks update-kubeconfig --name ${var.cluster_name} --region ${var.region}"
 }
 
 # Apply YAML kubernetes-manifest configurations
@@ -76,7 +42,7 @@ resource "null_resource" "apply_deployment" {
   }
 
   depends_on = [
-    module.gcloud
+    module.awscli
   ]
 }
 
